d12343d6aff018bcdfac5b0109a81036d72411d7
Restyled.io <commits@restyled.io>
2020-07-22 11:54:22 +0000

    Restyled by prettier

--- routes/freejobalert/route.js
+++ routes/freejobalert/route.js
@@ -1,44 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-var log = require('signale');
-const latestNotifications =
-    require('../../customModules/freejobalerts/scraper').latestNotifications;
-const topicScraper =
-    require('../../customModules/freejobalerts/scraper').topicScraper;
-const smartScraper =
-    require('../../customModules/freejobalerts/scraper').smartScraper;
-const stateCodes = require('../../data/freeJobAlertStateMap.json');
+var log = require("signale");
+const latestNotifications = require("../../customModules/freejobalerts/scraper")
+  .latestNotifications;
+const topicScraper = require("../../customModules/freejobalerts/scraper")
+  .topicScraper;
+const smartScraper = require("../../customModules/freejobalerts/scraper")
+  .smartScraper;
+const stateCodes = require("../../data/freeJobAlertStateMap.json");
 log.scope("freejobalert:global");
 
 /////////////////////////////////////////////////// routes
 /////////////////////////////////////////////////
-router.get('/', getDefault, (req, res) => { res.send(res.results); });
+router.get("/", getDefault, (req, res) => {
+  res.send(res.results);
+});
 
-router.get('/gov/other-all-india-exam', otherAllIndiaExam,
-           (req, res) => { res.send(res.results); });
+router.get("/gov/other-all-india-exam", otherAllIndiaExam, (req, res) => {
+  res.send(res.results);
+});
 
-router.get('/gov/state/:code([a-zA-Z]{2})', stateWiseGovjobs,
-           (req, res) => { res.send(res.results); })
+router.get("/gov/state/:code([a-zA-Z]{2})", stateWiseGovjobs, (req, res) => {
+  res.send(res.results);
+});
 
-router.get('/bank-jobs', bankJobs, (req, res) => { res.send(res.results); });
+router.get("/bank-jobs", bankJobs, (req, res) => {
+  res.send(res.results);
+});
 
-router.get('/teaching-jobs', allIndiaTeachingJobs,
-           (req, res) => { res.send(res.results); });
+router.get("/teaching-jobs", allIndiaTeachingJobs, (req, res) => {
+  res.send(res.results);
+});
 
-router.get('/engineering-jobs', allIndiaEngineeringJobs,
-           (req, res) => { res.send(res.results); });
+router.get("/engineering-jobs", allIndiaEngineeringJobs, (req, res) => {
+  res.send(res.results);
+});
 
-router.get('/railway-jobs', allIndiaRailwayJobs,
-           (req, res) => { res.send(res.results); });
+router.get("/railway-jobs", allIndiaRailwayJobs, (req, res) => {
+  res.send(res.results);
+});
 
-router.get('/defence-jobs', allIndiaDefenceJobs,
-           (req, res) => { res.send(res.results); });
+router.get("/defence-jobs", allIndiaDefenceJobs, (req, res) => {
+  res.send(res.results);
+});
 
 /////////////////////////////////////////////////////// Invalid URL
 ///////////////////////////////////////////
 
-router.all('*', notFound,
-           (req, res) => { res.json({err : "404: invlid URL"}); })
+router.all("*", notFound, (req, res) => {
+  res.json({ err: "404: invlid URL" });
+});
 
 ///////////////////////////////////////// middleware functions
 ////////////////////////////////////////////////
@@ -46,22 +57,23 @@ router.all('*', notFound,
 async function stateWiseGovjobs(req, res, next) {
   log = log.scope("freejobalert:stateWiseGovjobs");
   var stateCode = req.params.code.toUpperCase();
-  var state =
-      stateCodes.filter(function(item) { return item.code == stateCode; })[0];
+  var state = stateCodes.filter(function (item) {
+    return item.code == stateCode;
+  })[0];
   var URL = state.link;
   log.info(URL);
   const topic = "Govt. job for " + state.name;
   await smartScraper(URL, topic)
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = {err : "Something Went Wrong"};
-      })
-      .then((data) => {
-        log.success(data);
-        res.status(200);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { err: "Something Went Wrong" });
+    })
+    .then((data) => {
+      log.success(data);
+      res.status(200);
+      return (res.results = data);
+    });
   next();
 }
 
@@ -73,16 +85,16 @@ async function allIndiaDefenceJobs(req, res, next) {
   let results;
 
   await topicScraper(URL, topic, tableNo)
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = {err : "Something Went Wrong"};
-      })
-      .then((data) => {
-        log.success(data);
-        res.status(200);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { err: "Something Went Wrong" });
+    })
+    .then((data) => {
+      log.success(data);
+      res.status(200);
+      return (res.results = data);
+    });
   next();
 }
 
@@ -94,16 +106,16 @@ async function allIndiaRailwayJobs(req, res, next) {
   let results;
 
   await topicScraper(URL, topic, tableNo)
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = {err : "Something Went Wrong"};
-      })
-      .then((data) => {
-        log.success(data);
-        res.status(200);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { err: "Something Went Wrong" });
+    })
+    .then((data) => {
+      log.success(data);
+      res.status(200);
+      return (res.results = data);
+    });
   next();
 }
 
@@ -115,16 +127,16 @@ async function allIndiaEngineeringJobs(req, res, next) {
   let results;
 
   await topicScraper(URL, topic, tableNo)
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = {err : "Something Went Wrong"};
-      })
-      .then((data) => {
-        log.success(data);
-        res.status(200);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { err: "Something Went Wrong" });
+    })
+    .then((data) => {
+      log.success(data);
+      res.status(200);
+      return (res.results = data);
+    });
   next();
 }
 
@@ -136,16 +148,16 @@ async function bankJobs(req, res, next) {
   let results;
 
   await topicScraper(URL, topic, tableNo)
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = {err : "Something Went Wrong"};
-      })
-      .then((data) => {
-        log.success(data);
-        res.status(200);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { err: "Something Went Wrong" });
+    })
+    .then((data) => {
+      log.success(data);
+      res.status(200);
+      return (res.results = data);
+    });
   next();
 }
 
@@ -156,16 +168,16 @@ async function otherAllIndiaExam(req, res, next) {
   const tableNo = 4;
   let results;
   await topicScraper(URL, topic, tableNo)
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = { error : "Something Went Wrong" }
-      })
-      .then((data) => {
-        res.status(200);
-        log.success(data);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { error: "Something Went Wrong" });
+    })
+    .then((data) => {
+      res.status(200);
+      log.success(data);
+      return (res.results = data);
+    });
   next();
 }
 
@@ -176,16 +188,16 @@ async function allIndiaTeachingJobs(req, res, next) {
   const tableNo = 2;
   let results;
   await topicScraper(URL, topic, tableNo)
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = { error : "Something Went Wrong" }
-      })
-      .then((data) => {
-        res.status(200);
-        log.success(data);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { error: "Something Went Wrong" });
+    })
+    .then((data) => {
+      res.status(200);
+      log.success(data);
+      return (res.results = data);
+    });
   next();
 }
 
@@ -194,16 +206,16 @@ async function getDefault(req, res, next) {
   log = log.scope("freejobalert:getDefault");
   let results;
   await latestNotifications("http://www.freejobalert.com/")
-      .catch((error) => {
-        res.status(404);
-        log.error(error);
-        return res.results = { error : "Something Went Wrong" }
-      })
-      .then((data) => {
-        res.status(200);
-        log.success(data);
-        return res.results = data;
-      })
+    .catch((error) => {
+      res.status(404);
+      log.error(error);
+      return (res.results = { error: "Something Went Wrong" });
+    })
+    .then((data) => {
+      res.status(200);
+      log.success(data);
+      return (res.results = data);
+    });
 
   next();
 }
